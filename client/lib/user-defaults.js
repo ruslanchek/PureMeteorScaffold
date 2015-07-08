ceccd1662af80c361bfe5185f2280084abf241ba
Ruslan Shashkov <ruslan.shashkov@ibitcy.com>
2015-07-08 18:11:43 +0300

    User and bricks

--- client/lib/user-defaults.js
+++ client/lib/user-defaults.js
@@ -1,7 +1,7 @@
 var userDefaults = Meteor.settings.public.userDefaults;
 
 for(var item in userDefaults){
-    if(!Session.equals(item, userDefaults[item])){
-        Session.setDefault(item, userDefaults[item]);
+    if(!Session.get(item)){
+        Session.set(item, userDefaults[item]);
     }
 }
